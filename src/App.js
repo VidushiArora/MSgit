eca917e9ab0aededacb541d256aa3443459dd7d3
Vidushi_arora <vvidushiarora916@gmail.com>
2021-04-24 13:34:25 +0530

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+
+  const [inputList,setInputList] = useState("buy apple");
+  const [items,setItems]= useState([]);
+
+  const itemEvent=(event)=>{
+    setInputList(event.target.value);
+  };
+
+  const deleteItems = () => {
+    return(
+      <>
+      <p>Deleted</p>
+      </>
+    )
+  }
+
+  const listOfItems=()=>{
+    setItems((olditems) => {
+      return [...olditems, inputList];
+    });
+    setInputList("");
+  };
+  return(
+    <>
+    <div className="main_div">
+      <div className="center_div">
+        <br/>
+        <h1> TODO LIST</h1>
+        <br/>
+        <input type="text" placeholder="Add a Items" onChange={itemEvent} value={inputList} />
+        <button onClick={listOfItems}> + </button>
+
+        <ol>
+          {/* <li> {inputList} </li> */}
+          {
+          items.map((itemval)=> {
+            return (
+            <div className="todo_style">
+              <p class="fa fa-times" aria-hidden="true" onClick={deleteItems}>X</p>
+              <li>{itemval}</li></div>)
+          })
+        }
+        </ol>
+      </div>
     </div>
-  );
-}
+    </>
+  )
+};
 
 export default App;
